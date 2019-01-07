1f643ed166e0b801d77e85973f833c12c5c9d13e
zlove <zlove.zhang@bytedance.com>
2019-01-07 14:22:27 +0800

    Format code

--- flutter_redux/lib/main.dart
+++ flutter_redux/lib/main.dart
@@ -28,14 +28,13 @@ class LoginSuccessAction {
   LoginSuccessAction({this.account});
 }
 
-enum Actions{
+enum Actions {
   Increase,
   LogoutAction,
 }
 
-AppState mainReducer(AppState state, dynamic action){
-
-  if (Actions.Increase == action){
+AppState mainReducer(AppState state, dynamic action) {
+  if (Actions.Increase == action) {
     state.mainPageState.counter += 1;
   } else if (Actions.LogoutAction == action) {
     state.authState.isLogin = false;
@@ -49,41 +48,48 @@ AppState mainReducer(AppState state, dynamic action){
 }
 
 void main() {
-  Store<AppState> store = new Store<AppState>(mainReducer, initialState: AppState(authState: AuthState(), mainPageState: MainPageState()));
-  runApp(new MyApp(store: store,));
+  Store<AppState> store = new Store<AppState>(mainReducer,
+      initialState:
+          AppState(authState: AuthState(), mainPageState: MainPageState()));
+  runApp(new MyApp(
+    store: store,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-
   final Store<AppState> store;
 
   MyApp({this.store});
 
   @override
   Widget build(BuildContext context) {
-    return new StoreProvider(store: store, child: new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  new StoreConnector(builder: (BuildContext context, AppState appState){
-        return new MyHomePage(title: 'Flutter Demo Home Page', appState: appState,);
-      }, converter: (Store<AppState> store){
-        return store.state;
-      }) ,
-    ));
+    return new StoreProvider(
+        store: store,
+        child: new MaterialApp(
+          title: 'Flutter Demo',
+          theme: new ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: new StoreConnector(
+              builder: (BuildContext context, AppState appState) {
+            return new MyHomePage(
+              title: 'Flutter Demo Home Page',
+              appState: appState,
+            );
+          }, converter: (Store<AppState> store) {
+            return store.state;
+          }),
+        ));
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
-  MyHomePage({Key key, this.title,this.appState}) : super(key: key);
+  MyHomePage({Key key, this.title, this.appState}) : super(key: key);
   final String title;
   final AppState appState;
 
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(title),
@@ -97,23 +103,20 @@ class MyHomePage extends StatelessWidget {
             ),
             new Text(
               appState.mainPageState.counter.toString(),
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1,
+              style: Theme.of(context).textTheme.display1,
             ),
             _buildLoginButton(),
           ],
         ),
       ),
-      floatingActionButton: StoreConnector<AppState, VoidCallback>(builder: (BuildContext context,VoidCallback callback) {
+      floatingActionButton: StoreConnector<AppState, VoidCallback>(
+          builder: (BuildContext context, VoidCallback callback) {
         return FloatingActionButton(
-          onPressed:callback,
+          onPressed: callback,
           tooltip: 'Increment',
           child: Icon(Icons.add),
         );
-
-      }, converter: (Store<AppState> store){
+      }, converter: (Store<AppState> store) {
         return () => store.dispatch(Actions.Increase);
       }),
     );
@@ -126,21 +129,27 @@ class MyHomePage extends StatelessWidget {
       loginButton = StoreConnector(
           key: ValueKey("login"),
           builder: (BuildContext context, VoidCallback logout) {
-        return RaisedButton(onPressed: logout, child: Text("您好：$account，点击退出"),);
-      }, converter: (Store<AppState> store) {
-        return () => store.dispatch(Actions.LogoutAction);
-      });
+            return RaisedButton(
+              onPressed: logout,
+              child: Text("您好：$account，点击退出"),
+            );
+          },
+          converter: (Store<AppState> store) {
+            return () => store.dispatch(Actions.LogoutAction);
+          });
     } else {
       loginButton = StoreConnector(
           key: ValueKey("logout"),
           builder: (BuildContext context, VoidCallback login) {
-        return RaisedButton(onPressed: login, child: Text("登录"),);
-      }, converter: (Store<AppState> store) {
-        return () => store.dispatch(LoginSuccessAction(account: "zlove"));
-      });
+            return RaisedButton(
+              onPressed: login,
+              child: Text("登录"),
+            );
+          },
+          converter: (Store<AppState> store) {
+            return () => store.dispatch(LoginSuccessAction(account: "zlove"));
+          });
     }
     return loginButton;
   }
 }
-
-
